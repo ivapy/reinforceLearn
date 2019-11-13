855674db3b657d7201628fac086daaf0ea506104
SiddharthSalunkhe <siddharth.salunkhe11@gmail.com>
2019-11-13 18:41:29 -0500

    trying other implementations for actor critics

--- Agents.py
+++ Agents.py
@@ -95,6 +95,62 @@ class QLearningAgent():
 				if done:
 					observation = self.env.reset()
 				time.sleep(0.01)
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
 
 
 class DeepQAgent():
@@ -117,17 +173,17 @@ class DeepQAgent():
 
 		minibatch = random.sample(self.event_memory, batch_size)
 
-        for state, next_state, reward, action, terminated in minibatch:
+		for state, next_state, reward, action, terminated in minibatch:
 
-            target = self.policy.predict(state)
+			target = self.policy.predict(state)
 
-            if terminated:
-                target[0][action] = reward
-            else:
-                t = self.policy.predict(next_state)
-                target[0][action] = reward + self.gamma * np.argmax(t)
+			if terminated:
+				target[0][action] = reward
+			else:
+				t = self.policy.predict(next_state)
+				target[0][action] = reward + self.gamma * np.argmax(t)
 
-            self.policy.fit(state, target, epochs=1, verbose=0)
+			self.policy.fit(state, target, epochs=1, verbose=0)
 
 	def add_to_memory(self,curr_state,next_state,reward,action,done):
 		if len(self.event_memory) >= self.max_memory:
@@ -191,6 +247,30 @@ class DeepQAgent():
 					observation = self.env.reset()
 				time.sleep(0.01)
 
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
 
 
 class LinQuadLayer(layers.Layer):
@@ -215,6 +295,42 @@ class LinQuadLayer(layers.Layer):
 	def __triangle(self,N):
 		return (N**2 + N)/2
 
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+
 
 class ActorCritic():
 
@@ -238,16 +354,21 @@ class ActorCritic():
 		h3 = Dense(24, activation='relu', kernel_initializer='random_uniform', bias_initializer='zeros')(h2)
 		vel_output = Dense(2, activation='relu', kernel_initializer='random_uniform', bias_initializer='zeros')(h3)
 		ang_output = Dense(2, activation='relu', kernel_initializer='random_uniform', bias_initializer='zeros')(h3)
-		op_concat = Concatenate()([vel_output, ang_output])
-		output = Flatten()(op_concat)
-		model = Model(input=[state_input, delta], output=output)
+		vel_model = Model(input=[state_input, delta], output=vel_output)
+		ang_model = Model(input=[state_input, delta], output=ang_output)
 
-		action_placeholder = tf.placeholder(tf.float32)
-		delta_placeholder = tf.placeholder(tf.float32)
-		norm_dist = tf.placeholder(tfd.Normal(loc = 0,scale = 1))
-		self.loss_actor = -tf.log(norm_dist.prob(action_placeholder) + 1e-5) * delta_placeholder
+		vel_action_placeholder = tf.placeholder(tf.float32)
+		ang_action_placeholder = tf.placeholder(tf.float32)
+		vel_delta_placeholder = tf.placeholder(tf.float32)
+		ang_delta_placeholder = tf.placeholder(tf.float32)
+		vel_norm_dist = tf.placeholder(tfd.Normal(loc = 0,scale = 1))
+		ang_norm_dist = tf.placeholder(tfd.Normal(loc = 0,scale = 1))
+
+
+		self.loss_actor = -tf.log(ang_norm_dist.prob(ang_action_placeholder)*vel_norm_dist.prob(vel_action_placeholder) + 1e-5) * delta_placeholder
 		self.training_op_actor = tf.train.AdamOptimizer(lr_actor, name='actor_optimizer').minimize(self.loss_actor)
 		return model
+
 
 
 
@@ -278,8 +399,8 @@ class ActorCritic():
 				self.critic.fit([cur_state, action], reward, verbose=0)
 
 
-	def __train_actor(self, advantage, cur_state, next_state):
-		pass
+	def __train_actor(self, action ,advantage, cur_state, vel_dist, ang_dist):
+		self.sess.run([self.training_op_actor, self.loss_actor], feed_dict = {norm_dist: action_placeholder: action, delta_placeholder: advantage})
 
 	def train(self):
 		pass
@@ -295,7 +416,7 @@ class ActorCritic():
 		ang_dist = tfp.distributions.Normal(loc = mu_ang, scale = sigma_ang)
 		op_vel = tf.clip_by_value(vel_dist.sample(),self.env.action_space.low[0],self.env.action_space.high[0])
 		op_ang = tf.clip_by_value(ang_dist.sample(),self.env.action_space.low[1],self.env.action_space.high[1])
-		return op_vel, op_ang
+		return op_vel, op_ang, vel_dist, ang_dist
 
 
 	def update(self):
@@ -328,3 +449,148 @@ class ActorCritic():
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+
+class A2CAgent:
+	def __init__(self, state_size, action_size):
+		# if you want to see Cartpole learning, then change to True
+		self.render = False
+		self.load_model = False
+		self.state_size = state_size
+		self.action_size = action_size
+		self.value_size = 1
+
+		# get gym environment name
+		# these are hyper parameters for the A3C
+		self.actor_lr = 0.0001
+		self.critic_lr = 0.001
+		self.discount_factor = .9
+		self.hidden1, self.hidden2 = 24, 24
+
+		# create model for actor and critic network
+		self.actor, self.critic = self.build_model()
+
+		# method for training actor and critic network
+		self.optimizer = [self.actor_optimizer(), self.critic_optimizer()]
+
+		if self.load_model:
+			self.actor.load_weights("./save_model/cartpole_actor.h5")
+			self.critic.load_weights("./save_model/cartpole_critic.h5")
+
+	def build_model(self):
+		state = Input(batch_shape=(None, self.state_size))
+		actor_input = Dense(30, input_dim=self.state_size, activation='relu', kernel_initializer='he_uniform')(state)
+		# actor_hidden = Dense(self.hidden2, activation='relu')(actor_input)
+		mu_0 = Dense(self.action_size, activation='tanh', kernel_initializer='he_uniform')(actor_input)
+		sigma_0 = Dense(self.action_size, activation='softplus', kernel_initializer='he_uniform')(actor_input)
+
+		mu = Lambda(lambda x: x * 2)(mu_0)
+		sigma = Lambda(lambda x: x + 0.0001)(sigma_0)
+
+
+		critic_input = Input(shape = (2,), name = 'State_In')
+		action_input = Input(shape = (2,), name = 'Action_In')
+
+		sa_concat = Concatenate([state_input,action_input])
+		quadLayer = LinQuadLayer()(sa_concat)
+		fc_linear = Dense(1)(sa_concat)
+		fc_quad = Dense(1)(quadLayer)
+		addLayer = Add()([fc_linear, fc_quad])
+
+		reward_estimator = Model(inputs = [state_input, action_input], outputs = addLayer)
+
+
+		actor = Model(inputs=state, outputs=(mu, sigma))
+		critic = Model(inputs=critic_input, outputs=reward_estimator)
+
+		actor._make_predict_function()
+		critic._make_predict_function()
+
+		actor.summary()
+		critic.summary()
+
+		return actor, critic
+
+	def actor_optimizer(self):
+		action = K.placeholder(shape=(None, 1))
+		advantages = K.placeholder(shape=(None, 1))
+
+		# mu = K.placeholder(shape=(None, self.action_size))
+		# sigma_sq = K.placeholder(shape=(None, self.action_size))
+
+		mu, sigma_sq = self.actor.output
+
+		pdf = 1. / K.sqrt(2. * np.pi * sigma_sq) * K.exp(-K.square(action - mu) / (2. * sigma_sq))
+		log_pdf = K.log(pdf + K.epsilon())
+		entropy = K.sum(0.5 * (K.log(2. * np.pi * sigma_sq) + 1.))
+
+		exp_v = log_pdf * advantages
+
+		exp_v = K.sum(exp_v + 0.01 * entropy)
+		actor_loss = -exp_v
+
+		optimizer = Adam(lr=self.actor_lr)
+		updates = optimizer.get_updates(self.actor.trainable_weights, [], actor_loss)
+
+		train = K.function([self.actor.input, action, advantages], [], updates=updates)
+		return train
+
+	# make loss function for Value approximation
+	def critic_optimizer(self):
+		discounted_reward = K.placeholder(shape=(None, 1))
+
+		value = self.critic.output
+
+		loss = K.mean(K.square(discounted_reward - value))
+
+		optimizer = Adam(lr=self.critic_lr)
+		updates = optimizer.get_updates(self.critic.trainable_weights, [], loss)
+		train = K.function([self.critic.input, discounted_reward], [], updates=updates)
+		return train
+
+	# using the output of policy network, pick action stochastically
+	def get_action(self, state):
+		mu, sigma_sq = self.actor.predict(np.reshape(state, [1, self.state_size]))
+		# sigma_sq = np.log(np.exp(sigma_sq + 1))
+		epsilon = np.random.randn(self.action_size)
+		# action = norm.rvs(loc=mu, scale=sigma_sq,size=1)
+		action = mu + np.sqrt(sigma_sq) * epsilon
+		action = np.clip(action, -2, 2)
+		return action
+
+	# update policy network every episode
+	def train_model(self, state, action, reward, next_state, done):
+		target = np.zeros((1, self.value_size))
+		advantages = np.zeros((1, self.action_size))
+
+		value = self.critic.predict(state)[0]
+		next_value = self.critic.predict(next_state)[0]
+
+		if done:
+			advantages[0] = reward - value
+			target[0][0] = reward
+		else:
+			advantages[0] = reward + self.discount_factor * (next_value) - value
+			target[0][0] = reward + self.discount_factor * next_value
+
+		self.optimizer[0]([state, action, advantages])
+		self.optimizer[1]([[state, action], target])
+"""
